55b653697afd013185ea13d957a51f7c07028ffe
Junia George <jag529@cornell.edu>
2015-05-04 09:49:22 -0400

    added shuffle, evaluation for each time a new card(s) is/are flipped, and code to flip new cards (almost, still have to correct the order in which cards are flipped/bets are made but that will be easy). Realize that the tiebreaker struct needs to include the winning hand for future tiebreakers *sigh*

--- poker.c
+++ poker.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 typedef struct {
     unsigned int suit;
@@ -13,26 +15,59 @@ typedef struct {
 unsigned int x = 0;
 unsigned int y = 0;
 card deck[52];
+card player1[2];
+card player2[2];
+card table[5];
 
 unsigned int t = 0;
 unsigned int j = 0;
 
-void main(){
-	
+//flop
+void flop(){
+	table[0] = deck[4];
+	table[1] = deck[5];
+	table[2] = deck[6];
 }
 
+//turn
+void turn(){
+	table[3] = deck[8];	
+}
+
+//river
+void river(){
+	table[4] = deck[10];	
+}
+
+
+//create a deck of cards
 void create() {
 	for(x = 1; x <= 13; x++) {
 		for(y = 0; y <=3; y++){
 			deck[12*y+x-1].value = x;
-			deck[12*y+x-1].suit = y;
-			
+			deck[12*y+x-1].suit = y;	
 		}
 	}
 }
 
+//shuffle deck of cards; this is the fisher-yates shuffle (a method I found online)
+void shuffle()
+{
+	unsigned int i = 51;
+    unsigned int j;
+    card temp;
+	srand(time(NULL));
+    while (i > 0){
+    	j = rand() % (i + 1);
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+        i = i - 1;
+    }
+}
 
-tiebreaker hand(card hand[5]) {
+//input is an array of 5 cards in order of value (where aces have a value of 1). Out put is the type of hand you have
+tiebreaker pattern_match(card hand[5]) {
 	unsigned int isFlush = 1;
 	unsigned int isStraight = 0;
 	unsigned int foundAce = 0;
@@ -299,4 +334,96 @@ tiebreaker hand(card hand[5]) {
 		return win;
 	}
 	return win;
+}
+
+//n is the number of cards that have been flipped on the table plus 2
+tiebreaker optimal(unsigned int n, card inplay[7]){
+	unsigned int i=0, j=0, a;
+	card hand[5];
+	tiebreaker best, temp;
+	if(n == 5) {
+		for(a = 0; a < 5; a ++){
+			hand[a] = inplay[a];
+		}
+		best = pattern_match(hand);
+	}
+	else{
+		for(x = 0; x < n; x++){
+			if(n==7){
+				for(y = 0; y < n; y++){
+					if(x != y){
+						while(j < n){
+							if(i != x && i != y){
+								hand[j] = inplay[i];	
+								j++;
+							}	
+							i++;
+						}
+						temp = pattern_match(hand);
+						if(temp.rank > best.rank){
+							best = temp;
+						}
+						else if(temp.rank = best.rank){
+							if(temp.wincard > best.wincard){
+								best = temp;	
+							}	
+						}
+					}	
+				}
+			}
+			else {
+				while(j < n){
+					if(i != x){
+						hand[j] = inplay[i];	
+						j++;
+					}	
+					i++;
+				}
+				temp = pattern_match(hand);
+				if(temp.rank > best.rank){
+					best = temp;
+				}
+				else if(temp.rank = best.rank){
+					if(temp.wincard > best.wincard){
+						best = temp;	
+					}	
+				}	
+			}
+			
+		}
+	}
+	
+	return best;
+}	
+
+//n is the number of cards in play and p represents the player
+tiebreaker inplayarray(int p, int n){
+	card play[7];
+	
+	if(p == 1){
+		memcpy(play, player1, 2 * sizeof(float)); 
+	}
+	else {
+		memcpy(play, player2, 2 * sizeof(float));	
+	}
+	memcpy(play + 2, table, 5 * sizeof(float));
+	return optimal(n, play);
+}
+
+void main(){
+	create();
+	shuffle();
+	player1[0] = deck[0];
+	player1[1] = deck[2];
+	player2[0] = deck[1];
+	player2[1] = deck[3];
+	
+	//bet on cards (big blind small blind)
+	
+	//flop and bet
+	
+	//turn and bet
+	
+	//river	and bet
+	
 }
