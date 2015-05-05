64c290e5aebbea0ca8867a0d317bda5d3eaf11ad
agicsaki <agicsaki24@gmail.com>
2015-05-05 00:58:28 -0400

    revert to prevoius commit

--- poker.c
+++ poker.c
@@ -1,30 +1,114 @@
+/*-----------------------------------------------------------------------------
+	Agi Csaki (ac2289)
+	Junia George (jag529)
+	CS 3420 Final Project
+	This file contains the AI against which a player must play poker
+-----------------------------------------------------------------------------*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include "msp430x22x4.h"
 
+/*-----------------------------------------------------------------------------
+	Card Struct
+		Represents a single card
+		Suit: 0-3 representing spades, diamonds, clubs, hearts
+		Value: 1-13 representing the value of the card
+-----------------------------------------------------------------------------*/
 typedef struct {
     unsigned int suit;
     unsigned int value;
 } card;
 
+/*-----------------------------------------------------------------------------
+	Tiebreaker Struct
+		Represents the outcome of a round
+		Wincard: the highest card in the hand (can hold values 2-14)
+		Rank: represents highest ranking poker hand the AI has (can hold values
+		1-10 representing each unique type of poker hand)
+-----------------------------------------------------------------------------*/
 typedef struct {
     int wincard;
     int rank;
-    card winhand[5];
 } tiebreaker;
 
-unsigned int x = 0;
-unsigned int y = 0;
-card deck[52];
-card player1[2];
-card player2[2];
-card table[5];
+/*-----------------------------------------------------------------------------
+	Deck global variable
+		Represents the deck of cards being used
+		An array with 52 elements, ordering of card structs in the array 
+		represents order of cards in the deck
+		Invariant: each card struct in the deck is unique ensuring we simulate
+		gameplay with a full deck of cards
+-----------------------------------------------------------------------------*/
+card* deck[52];
 
+/*-----------------------------------------------------------------------------
+	Player global variables
+		Represent the hands of players 1 and 2
+		An array with 2 elements representing the 2 cards each player holds
+		Invariants: a card held by either player cannot also be contained in
+		deck, and these arrays contain the same cards on either device
+-----------------------------------------------------------------------------*/
+card* player1[2];
+card* player2[2];
+
+/*-----------------------------------------------------------------------------
+	Table global variable
+		Represents the cards played by the dealer
+		An array with 5 elements, representing the cards laid on the table by
+		the dealer
+		Invariants: a card laid on the table cannot also be contained in deck,
+		and this array contains the same cards on both devices
+-----------------------------------------------------------------------------*/
+card* table[5];
+
+/*-----------------------------------------------------------------------------
+	Dealer global variable
+		Represents which player is currently the dealer
+		Can hold value 1 or 2, corresponding to either player
+-----------------------------------------------------------------------------*/
+unsigned int dealer = 1;
+
+// Dummy variables to be used later in the program
 unsigned int t = 0;
 unsigned int j = 0;
+unsigned int x = 0;
+unsigned int y = 0;
 
-unsigned int dealer = 1;
+/*-----------------------------------------------------------------------------
+	Create Function
+		Initialize a full deck of cards, enforcing the invariant that each
+		card contained in the deck is unique
+-----------------------------------------------------------------------------*/
+void create() {
+	for(x = 1; x <= 13; x++) {
+		for(y = 0; y <=3; y++){
+			deck[12*y+x-1] = (card*)malloc(sizeof(card*));
+			deck[12*y+x-1]->value = x;
+			deck[12*y+x-1]->suit = y;	
+		}
+	}
+}
+
+/*-----------------------------------------------------------------------------
+	Shuffle Function
+		Shuffle the deck of cards using the Fisher-Yates shuffle
+		Maintains the invariant that the deck of cards contains 52 unique cards
+-----------------------------------------------------------------------------*/
+void shuffle()
+{
+	unsigned int i = 51;
+    unsigned int j;
+    card* temp;
+	srand(time(NULL));
+    while (i > 0){
+    	j = rand() % (i + 1);
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+        i = i - 1;
+    }
+}
 
 //flop
 void flop(){
@@ -41,33 +125,6 @@ void turn(){
 //river
 void river(){
 	table[4] = deck[10];	
-}
-
-
-//create a deck of cards
-void create() {
-	for(x = 1; x <= 13; x++) {
-		for(y = 0; y <=3; y++){
-			deck[12*y+x-1].value = x;
-			deck[12*y+x-1].suit = y;	
-		}
-	}
-}
-
-//shuffle deck of cards; this is the fisher-yates shuffle (a method I found online)
-void shuffle()
-{
-	unsigned int i = 51;
-    unsigned int j;
-    card temp;
-	srand(time(NULL));
-    while (i > 0){
-    	j = rand() % (i + 1);
-        temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-        i = i - 1;
-    }
 }
 
 //input is an array of 5 cards in order of value (where aces have a value of 1). Out put is the type of hand you have
@@ -104,7 +161,6 @@ tiebreaker pattern_match(card hand[5]) {
 	if(hand[0].value == 1 && (hand[4].value - hand[1].value == 3) && (hand[1].value == 10) && (isFlush == 1)) {
 		win.rank  = 10;
 		win.wincard = 14;
-		memcpy(win.winhand, hand, 5 * sizeof(card));
 		return win;
 	}
 	
@@ -112,7 +168,6 @@ tiebreaker pattern_match(card hand[5]) {
 	if(isStraight == 1 && isFlush == 1) {
 		win.rank  = 9;
 		win.wincard = hand[4].value;
-		memcpy(win.winhand, hand, 5 * sizeof(card));
 		return win;
 	}
 	
@@ -121,13 +176,11 @@ tiebreaker pattern_match(card hand[5]) {
 		if(foundAce) {
 			win.rank  = 6;
 			win.wincard = 14;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 		else{
 			win.rank  = 6;
 			win.wincard = hand[4].value;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 	}
@@ -136,12 +189,10 @@ tiebreaker pattern_match(card hand[5]) {
 		if(hand[0].value == 1 && (hand[4].value - hand[1].value == 3) && (hand[1].value == 10)){
 			win.rank  = 5;
 			win.wincard = 14;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 		win.rank  = 5;
 		win.wincard = hand[4].value;
-		memcpy(win.winhand, hand, 5 * sizeof(card));
 		return win;
 	}
 	
@@ -153,12 +204,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[0].value == 1){
 					win.rank  = 8;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 8;
 				win.wincard = hand[0].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else if(hand[3].value == hand[4].value){
@@ -166,12 +215,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[0].value == 1){
 					win.rank  = 7;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 7;
 				win.wincard = hand[0].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else{
@@ -179,12 +226,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[0].value == 1){
 					win.rank  = 4;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 4;
 				win.wincard = hand[0].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 		}
@@ -194,12 +239,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[2].value == 1){
 					win.rank  = 7;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 7;
 				win.wincard = hand[0].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else{
@@ -207,19 +250,16 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[0].value == 1 || hand[2].value == 1){
 					win.rank  = 3;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				if(hand[0].value < hand[2].value){
 					win.rank  = 3;
 					win.wincard = hand[2].value;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				else {
 					win.rank  = 3;
 					win.wincard = hand[0].value;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 			}
@@ -229,19 +269,16 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[0].value == 1 || hand[3].value == 1){
 				win.rank  = 3;
 				win.wincard = 14;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			if(hand[0].value < hand[3].value){
 				win.rank  = 3;
 				win.wincard = hand[3].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else{
 				win.rank  = 3;
 				win.wincard = hand[0].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 		}
@@ -250,12 +287,10 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[0].value == 1){
 				win.rank  = 2;
 				win.wincard = 14;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			win.rank  = 2;
 			win.wincard = hand[0].value;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 	}
@@ -266,12 +301,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[1].value == 1){
 					win.rank  = 8;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 8;
 				win.wincard = hand[1].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else{
@@ -279,12 +312,10 @@ tiebreaker pattern_match(card hand[5]) {
 				if(hand[1].value == 1){
 					win.rank  = 4;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 				}
 				win.rank  = 4;
 				win.wincard = hand[1].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 		}
@@ -293,19 +324,16 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[1].value == 1 || hand[3].value == 1){
 				win.rank  = 3;
 				win.wincard = 14;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			if(hand[1].value < hand[3].value){
 				win.rank  = 3;
 				win.wincard = hand[3].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			else{
 				win.rank  = 3;
 				win.wincard = hand[1].value;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 		}
@@ -314,12 +342,10 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[1].value == 1){
 				win.rank  = 2;
 				win.wincard = 14;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			win.rank  = 2;
 			win.wincard = hand[1].value;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 	}
@@ -329,12 +355,10 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[2].value == 1){
 				win.rank  = 4;
 					win.wincard = 14;
-					memcpy(win.winhand, hand, 5 * sizeof(card));
 					return win;
 			}
 			win.rank  = 4;
 			win.wincard = hand[2].value;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 		else{
@@ -342,12 +366,10 @@ tiebreaker pattern_match(card hand[5]) {
 			if(hand[2].value == 1){
 				win.rank  = 2;
 				win.wincard = 14;
-				memcpy(win.winhand, hand, 5 * sizeof(card));
 				return win;
 			}
 			win.rank  = 2;
 			win.wincard = hand[2].value;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 	}
@@ -356,24 +378,20 @@ tiebreaker pattern_match(card hand[5]) {
 		if(hand[3].value == 1){
 			win.rank  = 2;
 			win.wincard = 14;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 		win.rank  = 2;
 		win.wincard = hand[3].value;
-		memcpy(win.winhand, hand, 5 * sizeof(card));
 		return win;
 	}
 	else{
 		if(foundAce){
 			win.rank  = 1;
 			win.wincard = 14;
-			memcpy(win.winhand, hand, 5 * sizeof(card));
 			return win;
 		}
 		win.rank  = 1;
 		win.wincard = hand[5].value;
-		memcpy(win.winhand, hand, 5 * sizeof(card));
 		return win;
 	}
 	return win;
@@ -453,57 +471,8 @@ tiebreaker inplayarray(int p, int n){
 	return optimal(n, play);
 }
 
-
-/*------------------------------------------------------------------------
- * UART communication functions
- *----------------------------------------------------------------------*/
-
-/* Initialize the UART for TX (9600, 8N1) */
-/* Settings taken from TI UART demo */ 
-void init_uart(void) {
-	BCSCTL1 = CALBC1_1MHZ;        /* Set DCO for 1 MHz */
-	DCOCTL  = CALDCO_1MHZ;
-	P3SEL = 0x30;                 /* P3.4,5 = USCI_A0 TXD/RXD */
-	UCA0CTL1 |= UCSSEL_2;         /* SMCLK */
-	UCA0BR0 = 104;                /* 1MHz 9600 */
-	UCA0BR1 = 0;                  /* 1MHz 9600 */
-	UCA0MCTL = UCBRS0;            /* Modulation UCBRSx = 1 */
-	UCA0CTL1 &= ~UCSWRST;         /* Initialize USCI state machine */
-}
-
-/* Transmit a single character over UART interface */
-void uart_putc(char c) {
-    while(!(IFG2 & UCA0TXIFG)); /* Wait for TX buffer to empty */
-    UCA0TXBUF = c;				/* Transmit character */
-}
-
-/* Transmit a nul-terminated string over UART interface */
-void uart_puts(char *str) {
-	while (*str) {
-		/* Replace newlines with \r\n carriage return */
-		if(*str == '\n') { uart_putc('\r'); }
-		uart_putc(*str++);
-	}
-}
-
-/* Clear terminal screen using VT100 commands */
-/* http://braun-home.net/michael/info/misc/VT100_commands.htm */
-void uart_clear_screen(void) {
-	uart_putc(0x1B);		/* Escape character */
- 	uart_puts("[2J");		/* Clear screen */
- 	uart_putc(0x1B);		/* Escape character */
- 	uart_puts("[0;0H");		/* Move cursor to 0,0 */
-}
-
-/*-------------------------------------------------------------------------*/
-
 void main(){
-	init_uart();	/* Setup UART (Note: Sets clock to 1 MHz) */
-	
-	uart_puts("Creating deck of cards...\n\n");
 	create();
-	
-	uart_puts("Shuffling deck of cards...\n\n");
 	shuffle();
 	
 	if(dealer == 1){
