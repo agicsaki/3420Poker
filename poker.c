4fd6ded21931f4b33d11dc8d3b590e3eaa0f267d
agicsaki <agicsaki24@gmail.com>
2015-05-05 01:25:09 -0400

    tiebreakers and hand arrays all use pointers

--- poker.c
+++ poker.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "msp430x22x4.h"
 
 /*-----------------------------------------------------------------------------
 	Card Struct
@@ -127,293 +128,297 @@ void river(){
 	table[4] = deck[10];	
 }
 
-//input is an array of 5 cards in order of value (where aces have a value of 1). Out put is the type of hand you have
-tiebreaker pattern_match(card hand[5]) {
+//input is an array of 5 cards in order of value (where aces have a value of 1)-> Out put is the type of hand you have
+tiebreaker* pattern_match(card* hand[5]) {
 	unsigned int isFlush = 1;
 	unsigned int isStraight = 0;
 	unsigned int foundAce = 0;
 	
-	tiebreaker win = {0, 0};
-	
-	//check if flush. set isFlush to 1 if it is
 	unsigned int i = 1;
-	unsigned int temp = hand[0].suit;
+	unsigned int temp = hand[0]->suit;
+	
+	tiebreaker* win = (tiebreaker*)malloc(sizeof(tiebreaker*));
+	win->rank = 0;
+	win->wincard = 0;
+	
+	//check if flush-> set isFlush to 1 if it is
 	while(isFlush == 1 && i < 5) {
-		if(temp != hand[i].suit) {
+		if(temp != hand[i]->suit) {
 			isFlush = 0;
 		}
 		else i++;
 		
 	}	
 	//check if straight
-	if(hand[4].value - hand[0].value == 4) {
+	if(hand[4]->value - hand[0]->value == 4) {
 		isStraight = 1;
 	}
 	
 	//check if there is an ace
 	while(foundAce == 0 && j<5){
-		if(hand[j].value == 1){
+		if(hand[j]->value == 1){
 			foundAce=1;
 		}
 	}
 	
 	//check if royalFlush
-	if(hand[0].value == 1 && (hand[4].value - hand[1].value == 3) && (hand[1].value == 10) && (isFlush == 1)) {
-		win.rank  = 10;
-		win.wincard = 14;
+	if(hand[0]->value == 1 && (hand[4]->value - hand[1]->value == 3) && (hand[1]->value == 10) && (isFlush == 1)) {
+		win->rank  = 10;
+		win->wincard = 14;
 		return win;
 	}
 	
 	//straight flush
 	if(isStraight == 1 && isFlush == 1) {
-		win.rank  = 9;
-		win.wincard = hand[4].value;
+		win->rank  = 9;
+		win->wincard = hand[4]->value;
 		return win;
 	}
 	
 	//flush
 	if(isFlush){
 		if(foundAce) {
-			win.rank  = 6;
-			win.wincard = 14;
+			win->rank  = 6;
+			win->wincard = 14;
 			return win;
 		}
 		else{
-			win.rank  = 6;
-			win.wincard = hand[4].value;
+			win->rank  = 6;
+			win->wincard = hand[4]->value;
 			return win;
 		}
 	}
 	
 	if(isStraight){
-		if(hand[0].value == 1 && (hand[4].value - hand[1].value == 3) && (hand[1].value == 10)){
-			win.rank  = 5;
-			win.wincard = 14;
+		if(hand[0]->value == 1 && (hand[4]->value - hand[1]->value == 3) && (hand[1]->value == 10)){
+			win->rank  = 5;
+			win->wincard = 14;
 			return win;
 		}
-		win.rank  = 5;
-		win.wincard = hand[4].value;
+		win->rank  = 5;
+		win->wincard = hand[4]->value;
 		return win;
 	}
 	
 	//all other hands
-	if(hand[0].value == hand[1].value){
-		if(hand[1].value == hand[2].value){
-			if(hand[2].value == hand[3].value){
+	if(hand[0]->value == hand[1]->value){
+		if(hand[1]->value == hand[2]->value){
+			if(hand[2]->value == hand[3]->value){
 				//return 4 of a kind
-				if(hand[0].value == 1){
-					win.rank  = 8;
-					win.wincard = 14;
+				if(hand[0]->value == 1){
+					win->rank  = 8;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 8;
-				win.wincard = hand[0].value;
+				win->rank  = 8;
+				win->wincard = hand[0]->value;
 				return win;
 			}
-			else if(hand[3].value == hand[4].value){
+			else if(hand[3]->value == hand[4]->value){
 				//return full house
-				if(hand[0].value == 1){
-					win.rank  = 7;
-					win.wincard = 14;
+				if(hand[0]->value == 1){
+					win->rank  = 7;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 7;
-				win.wincard = hand[0].value;
+				win->rank  = 7;
+				win->wincard = hand[0]->value;
 				return win;
 			}
 			else{
 			//return triple
-				if(hand[0].value == 1){
-					win.rank  = 4;
-					win.wincard = 14;
+				if(hand[0]->value == 1){
+					win->rank  = 4;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 4;
-				win.wincard = hand[0].value;
+				win->rank  = 4;
+				win->wincard = hand[0]->value;
 				return win;
 			}
 		}
-		else if(hand[2].value == hand[3].value) {
-			if(hand[3].value == hand[4].value){
+		else if(hand[2]->value == hand[3]->value) {
+			if(hand[3]->value == hand[4]->value){
 				//return full house
-				if(hand[2].value == 1){
-					win.rank  = 7;
-					win.wincard = 14;
+				if(hand[2]->value == 1){
+					win->rank  = 7;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 7;
-				win.wincard = hand[0].value;
+				win->rank  = 7;
+				win->wincard = hand[0]->value;
 				return win;
 			}
 			else{
 				//return 2 pair
-				if(hand[0].value == 1 || hand[2].value == 1){
-					win.rank  = 3;
-					if(hand[0].value == 1){
-						win.wincard = 1400 + hand[2].value;
+				if(hand[0]->value == 1 || hand[2]->value == 1){
+					win->rank  = 3;
+					if(hand[0]->value == 1){
+						win->wincard = 1400 + hand[2]->value;
 					}
-					else win.wincard = 1400 + hand[0].value;
+					else win->wincard = 1400 + hand[0]->value;
 					return win;
 				}
-				if(hand[0].value < hand[2].value){
-					win.rank  = 3;
-					win.wincard = hand[2].value*100 + hand[0].value;
+				if(hand[0]->value < hand[2]->value){
+					win->rank  = 3;
+					win->wincard = hand[2]->value*100 + hand[0]->value;
 					return win;
 				}
 				else {
-					win.rank  = 3;
-					win.wincard = hand[0].value*100 + hand[2].value;
+					win->rank  = 3;
+					win->wincard = hand[0]->value*100 + hand[2]->value;
 					return win;
 				}
 			}
 		}
-		else if(hand[3].value == hand[4].value){
+		else if(hand[3]->value == hand[4]->value){
 			//return 2 pair
-			if(hand[0].value == 1 || hand[3].value == 1){
-				win.rank  = 3;
-				if(hand[0].value == 1){
-						win.wincard = 1400 + hand[3].value;
+			if(hand[0]->value == 1 || hand[3]->value == 1){
+				win->rank  = 3;
+				if(hand[0]->value == 1){
+						win->wincard = 1400 + hand[3]->value;
 					}
-					else win.wincard = 1400 + hand[0].value;
+					else win->wincard = 1400 + hand[0]->value;
 				return win;
 			}
-			if(hand[0].value < hand[3].value){
-				win.rank  = 3;
-				win.wincard = hand[3].value*100 + hand[0].value;
+			if(hand[0]->value < hand[3]->value){
+				win->rank  = 3;
+				win->wincard = hand[3]->value*100 + hand[0]->value;
 				return win;
 			}
 			else{
-				win.rank  = 3;
-				win.wincard = hand[0].value*100 + hand[3].value;
+				win->rank  = 3;
+				win->wincard = hand[0]->value*100 + hand[3]->value;
 				return win;
 			}
 		}
 		else{
 			//return pair
-			if(hand[0].value == 1){
-				win.rank  = 2;
-				win.wincard = 14;
+			if(hand[0]->value == 1){
+				win->rank  = 2;
+				win->wincard = 14;
 				return win;
 			}
-			win.rank  = 2;
-			win.wincard = hand[0].value;
+			win->rank  = 2;
+			win->wincard = hand[0]->value;
 			return win;
 		}
 	}
-	else if (hand[1].value == hand[2].value){
-		if(hand[2].value == hand[3].value){
-			if(hand[3].value == hand[4].value){
+	else if (hand[1]->value == hand[2]->value){
+		if(hand[2]->value == hand[3]->value){
+			if(hand[3]->value == hand[4]->value){
 				//return 4 of a kind
-				if(hand[1].value == 1){
-					win.rank  = 8;
-					win.wincard = 14;
+				if(hand[1]->value == 1){
+					win->rank  = 8;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 8;
-				win.wincard = hand[1].value;
+				win->rank  = 8;
+				win->wincard = hand[1]->value;
 				return win;
 			}
 			else{
 				//return triple
-				if(hand[1].value == 1){
-					win.rank  = 4;
-					win.wincard = 14;
+				if(hand[1]->value == 1){
+					win->rank  = 4;
+					win->wincard = 14;
 					return win;
 				}
-				win.rank  = 4;
-				win.wincard = hand[1].value;
+				win->rank  = 4;
+				win->wincard = hand[1]->value;
 				return win;
 			}
 		}
-		else if(hand[3].value == hand[4].value) {
+		else if(hand[3]->value == hand[4]->value) {
 				//return 2 pair
-			if(hand[1].value == 1 || hand[3].value == 1){
-				win.rank  = 3;
-				if(hand[1].value == 1){
-					win.wincard = 1400 + hand[3].value;
+			if(hand[1]->value == 1 || hand[3]->value == 1){
+				win->rank  = 3;
+				if(hand[1]->value == 1){
+					win->wincard = 1400 + hand[3]->value;
 				}
-				else win.wincard = 1400 + hand[1].value;
+				else win->wincard = 1400 + hand[1]->value;
 				return win;
 			}
-			if(hand[1].value < hand[3].value){
-				win.rank  = 3;
-				win.wincard = hand[3].value*100 + hand[1].value;
+			if(hand[1]->value < hand[3]->value){
+				win->rank  = 3;
+				win->wincard = hand[3]->value*100 + hand[1]->value;
 				return win;
 			}
 			else{
-				win.rank  = 3;
-				win.wincard = hand[1].value*100 + hand[3].value;
+				win->rank  = 3;
+				win->wincard = hand[1]->value*100 + hand[3]->value;
 				return win;
 			}
 		}
 		else{
 			//return pair
-			if(hand[1].value == 1){
-				win.rank  = 2;
-				win.wincard = 14;
+			if(hand[1]->value == 1){
+				win->rank  = 2;
+				win->wincard = 14;
 				return win;
 			}
-			win.rank  = 2;
-			win.wincard = hand[1].value;
+			win->rank  = 2;
+			win->wincard = hand[1]->value;
 			return win;
 		}
 	}
-	else if (hand[2].value == hand[3].value){
-		if(hand[3].value == hand[4].value){
+	else if (hand[2]->value == hand[3]->value){
+		if(hand[3]->value == hand[4]->value){
 			//return triple
-			if(hand[2].value == 1){
-				win.rank  = 4;
-					win.wincard = 14;
-					return win;
+			if(hand[2]->value == 1){
+				win->rank  = 4;
+				win->wincard = 14;
+				return win;
 			}
-			win.rank  = 4;
-			win.wincard = hand[2].value;
+			win->rank  = 4;
+			win->wincard = hand[2]->value;
 			return win;
 		}
 		else{
 			//return pair
-			if(hand[2].value == 1){
-				win.rank  = 2;
-				win.wincard = 14;
+			if(hand[2]->value == 1){
+				win->rank  = 2;
+				win->wincard = 14;
 				return win;
 			}
-			win.rank  = 2;
-			win.wincard = hand[2].value;
+			win->rank  = 2;
+			win->wincard = hand[2]->value;
 			return win;
 		}
 	}
-	else if(hand[3].value == hand[4].value){
+	else if(hand[3]->value == hand[4]->value){
 		//return pair
-		if(hand[3].value == 1){
-			win.rank  = 2;
-			win.wincard = 14;
+		if(hand[3]->value == 1){
+			win->rank  = 2;
+			win->wincard = 14;
 			return win;
 		}
-		win.rank  = 2;
-		win.wincard = hand[3].value;
+		win->rank  = 2;
+		win->wincard = hand[3]->value;
 		return win;
 	}
 	else{
 		if(foundAce){
-			win.rank  = 1;
-			win.wincard = 14;
+			win->rank  = 1;
+			win->wincard = 14;
 			return win;
 		}
-		win.rank  = 1;
-		win.wincard = hand[5].value;
+		win->rank  = 1;
+		win->wincard = hand[5]->value;
 		return win;
 	}
 	return win;
 }
 
 //n is the number of cards that have been flipped on the table plus 2
-tiebreaker optimal(unsigned int n, card inplay[7]){
+tiebreaker* optimal(unsigned int n, card* inplay[7]){
 	unsigned int i=0, j=0, a;
-	card hand[5];
+	card* hand[5];
 	
 	
 	
-	tiebreaker best, temp;
+	tiebreaker* best;
+	tiebreaker* temp;
 	if(n == 5) {
 		for(a = 0; a < 5; a ++){
 			hand[a] = inplay[a];
@@ -433,11 +438,11 @@ tiebreaker optimal(unsigned int n, card inplay[7]){
 							i++;
 						}
 						temp = pattern_match(hand);
-						if(temp.rank > best.rank){
+						if(temp->rank > best->rank){
 							best = temp;
 						}
-						else if(temp.rank = best.rank){
-							if(temp.wincard > best.wincard){
+						else if(temp->rank = best->rank){
+							if(temp->wincard > best->wincard){
 								best = temp;	
 							}	
 						}
@@ -453,11 +458,11 @@ tiebreaker optimal(unsigned int n, card inplay[7]){
 					i++;
 				}
 				temp = pattern_match(hand);
-				if(temp.rank > best.rank){
+				if(temp->rank > best->rank){
 					best = temp;
 				}
-				else if(temp.rank = best.rank){
-					if(temp.wincard > best.wincard){
+				else if(temp->rank = best->rank){
+					if(temp->wincard > best->wincard){
 						best = temp;	
 					}	
 				}	
@@ -470,8 +475,8 @@ tiebreaker optimal(unsigned int n, card inplay[7]){
 }	
 
 //n is the number of cards in play and p represents the player
-tiebreaker inplayarray(int p, int n){
-	card play[7];
+tiebreaker* inplayarray(int p, int n){
+	card* play[7];
 	
 	if(p == 1){
 		memcpy(play, player1, 2 * sizeof(card)); 
@@ -492,7 +497,7 @@ tiebreaker inplayarray(int p, int n){
 void init_uart(void) {
 	BCSCTL1 = CALBC1_1MHZ;        /* Set DCO for 1 MHz */
 	DCOCTL  = CALDCO_1MHZ;
-	P3SEL = 0x30;                 /* P3.4,5 = USCI_A0 TXD/RXD */
+	P3SEL = 0x30;                 /* P3->4,5 = USCI_A0 TXD/RXD */
 	UCA0CTL1 |= UCSSEL_2;         /* SMCLK */
 	UCA0BR0 = 104;                /* 1MHz 9600 */
 	UCA0BR1 = 0;                  /* 1MHz 9600 */
@@ -516,7 +521,7 @@ void uart_puts(char *str) {
 }
 
 /* Clear terminal screen using VT100 commands */
-/* http://braun-home.net/michael/info/misc/VT100_commands.htm */
+/* http://braun-home->net/michael/info/misc/VT100_commands->htm */
 void uart_clear_screen(void) {
 	uart_putc(0x1B);		/* Escape character */
  	uart_puts("[2J");		/* Clear screen */
